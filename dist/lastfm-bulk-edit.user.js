ca5eb3fb3c2b24594435ff2d02a5f7bf0b81910d
Matt Bennett_ <matt.bennett@enterbridge.com>
2025-10-03 22:02:45 -0400

    fix: update github username in userscript config

--- dist/lastfm-bulk-edit.user.js
+++ dist/lastfm-bulk-edit.user.js
@@ -3,15 +3,15 @@
 // @description Bulk edit your scrobbles for any artist or album on Last.fm at once.
 // @version 1.6.2
 // @author Rudey
-// @homepage https://github.com/RudeySH/lastfm-bulk-edit
-// @supportURL https://github.com/RudeySH/lastfm-bulk-edit/issues
+// @homepage https://github.com/mxttbennett/lastfm-bulk-edit
+// @supportURL https://github.com/mxttbennett/lastfm-bulk-edit/issues
 // @match https://www.last.fm/*
-// @downloadURL https://raw.githubusercontent.com/RudeySH/lastfm-bulk-edit/main/dist/lastfm-bulk-edit.user.js
-// @icon https://raw.githubusercontent.com/RudeySH/lastfm-bulk-edit/main/img/icon.png
+// @downloadURL https://raw.githubusercontent.com/mxttbennett/lastfm-bulk-edit/main/dist/lastfm-bulk-edit.user.js
+// @icon https://raw.githubusercontent.com/mxttbennett/lastfm-bulk-edit/main/img/icon.png
 // @license AGPL-3.0-or-later
-// @namespace https://github.com/RudeySH/lastfm-bulk-edit
+// @namespace https://github.com/mxttbennett/lastfm-bulk-edit
 // @require https://cdnjs.cloudflare.com/ajax/libs/he/1.2.0/he.min.js
-// @updateURL https://raw.githubusercontent.com/RudeySH/lastfm-bulk-edit/main/dist/lastfm-bulk-edit.meta.js
+// @updateURL https://raw.githubusercontent.com/mxttbennett/lastfm-bulk-edit/main/dist/lastfm-bulk-edit.meta.js
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
